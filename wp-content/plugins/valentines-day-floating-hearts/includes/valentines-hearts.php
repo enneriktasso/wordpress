8a48b45c92a06b5dcacc42789ea07be73e32d24d
tassoennerikiktk <enneriktasso@gmail.com>
2020-01-27 23:15:51 +0200

    modified plugin `Valentine's Day Hearts` version 2.0 (after activation of `Sidebar Image Banner Ads Widget` version 1.0.2)

--- wp-content/plugins/valentines-day-floating-hearts/includes/valentines-hearts.php
+++ wp-content/plugins/valentines-day-floating-hearts/includes/valentines-hearts.php
@@ -18,7 +18,7 @@ function dc_valentines_heart() {
 	<!-- xmas_heart [ start ] -->
 	<script type="text/javascript">
 	// Set the number of heart (more than 30 - 40 not recommended)
-	var heartmax=20
+	var heartmax=30
 	// Set the colors for the heart. Add as many colors as you like
 	var heartcolor=new Array("#c6041b", "#e10027", "#E1001c", "#C10227", "#Ea002d", "#ed1c38")
 	// Set the fonts, that create the heartflakes. Add as many fonts as you like
@@ -26,9 +26,9 @@ function dc_valentines_heart() {
 	// Set the letter that creates your heartflake (recommended: * )
 	var heartletter="<i class='dcfa dcfa-heart' style='color:"+heartcolor[randommaker(heartcolor.length)]+" !important;' ></i>"
 	// Set the speed of sinking (recommended values range from 0.3 to 2)
-	var sinkspeed=0.6
+	var sinkspeed=0.8
 	// Set the maximum-size of your heartflakes
-	var heartmaxsize=30
+	var heartmaxsize=40
 	// Set the minimal-size of your heartflakes
 	var heartminsize=8
 	// Set the hearting-zone
